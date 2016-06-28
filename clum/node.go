3d5e2d5ef7dcffdf5097327066f5e156119856aa
Alex Egger <alex.egger96@gmail.com>
2016-06-28 12:22:23 +0200

    Add Run method to handle clients

--- clum/node.go
+++ clum/node.go
@@ -1,9 +1,15 @@
 package clum
 
 import (
+	"errors"
 	"net"
 
 	"github.com/nu7hatch/gouuid"
+)
+
+const (
+	// MaxConnFailures defines how many connection failures may happen, before the node terminates execution.
+	maxConnFailures = 5
 )
 
 // Node is the representation of a node in the cluster.
@@ -12,6 +18,8 @@ type Node struct {
 
 	Host string
 	net.Listener
+
+	stop chan bool
 }
 
 // New creates a new Node that listens on the address host.
@@ -25,6 +33,8 @@ func New(host string) (node *Node, err error) {
 		ID: *id,
 
 		Host: host,
+
+		stop: make(chan bool, 1),
 	}
 
 	if node.Listener, err = net.Listen("tcp", host); err != nil {
@@ -32,4 +42,32 @@ func New(host string) (node *Node, err error) {
 	}
 
 	return node, nil
+}
+
+func (node *Node) Run() (err error) {
+	failCounter := 0
+loop:
+	for {
+		select {
+		case <-node.stop:
+			break loop
+		default:
+			if failCounter >= maxConnFailures {
+				err = errors.New("Maximum amount of connection failures exceeded.")
+				break loop
+			}
+			conn, err := node.Accept()
+			if err != nil {
+				failCounter++
+				continue
+			}
+			go func(conn net.Conn) {
+				defer conn.Close()
+				// Handle client here.
+			}(conn)
+		}
+	}
+	// Close the stop channel.
+	close(node.stop)
+	return err
 }
