45544c13da20804d51c2a1a82f3d711e9bf70786
Alex Egger <alex.egger96@gmail.com>
2016-07-02 16:24:43 +0200

    Remove some fields, rename eventType to Operation, add operations

--- clum/message.go
+++ clum/message.go
@@ -2,28 +2,30 @@ package clum
 
 import "github.com/nu7hatch/gouuid"
 
-type eventType uint
+type Operation uint
 
 const (
 	// Join means the node is joining the cluster.
-	Join eventType = iota
+	Join Operation = iota
 	// Leave means the node is leaving the cluster.
 	Leave
 	// Transfer means the node is transfering its members to the peer.
 	Transfer
+	// NotifyJoin means the node is notifying another node of a join that happened.
+	NotifyJoin
+	// NotifyLeave means the node is notifying another node of a leave that happened.
+	NotifyLeave
 )
 
 // Event is the representation of an event caused by a node.
 type Event struct {
-	SenderID uuid.UUID
-	Event    eventType
+	SenderID  uuid.UUID
+	Operation Operation
 
 	Origin Member
 
-	Members []Member
+	Members []*Member
 
 	Time uint64
 	Hops uint
-
-	TransferRequired bool
 }
