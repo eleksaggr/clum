25d623eaf3d299c85b9579fbb2da371207662940
Alex Egger <alex.egger96@gmail.com>
2016-07-02 16:37:22 +0200

    Add EventQueue

--- clum/event.go
+++ clum/event.go
@@ -1,6 +1,10 @@
 package clum
 
-import "github.com/nu7hatch/gouuid"
+import (
+	"sync"
+
+	"github.com/nu7hatch/gouuid"
+)
 
 type Operation uint
 
@@ -28,4 +32,32 @@ type Event struct {
 
 	Time uint64
 	Hops uint
+}
+
+// EventQueue is a thread-safe queue holding events.
+type EventQueue struct {
+	events []Event
+	mutex  *sync.Mutex
+}
+
+// Enqueue adds an event to the tail of the queue.
+func (e *EventQueue) Enqueue(event Event) {
+	e.mutex.Lock()
+	e.events = append(e.events, event)
+	e.mutex.Unlock()
+}
+
+// Dequeue gets an element from the head of the queue and removes it.
+func (e *EventQueue) Dequeue() (event Event) {
+	e.mutex.Lock()
+	event = e.events[0]
+	e.events = e.events[1:]
+	e.mutex.Unlock()
+
+	return event
+}
+
+// Length returns the amount of events currently in the queue.
+func (e *EventQueue) Length() int {
+	return len(e.events)
 }
