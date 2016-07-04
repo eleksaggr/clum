8fd5d6ca8dd9e2ce84a192d9eab126c238e8dc30
Alex Egger <alex.egger96@gmail.com>
2016-07-04 12:53:59 +0200

    Unexport operation, remove hops, remove EventQueue

--- clum/event.go
+++ clum/event.go
@@ -1,16 +1,12 @@
 package clum
 
-import (
-	"sync"
+import "github.com/nu7hatch/gouuid"
 
-	"github.com/nu7hatch/gouuid"
-)
-
-type Operation uint
+type operation uint
 
 const (
 	// Join means the node is joining the cluster.
-	Join Operation = iota
+	Join operation = iota
 	// Leave means the node is leaving the cluster.
 	Leave
 	// Transfer means the node is transfering its members to the peer.
@@ -24,40 +20,11 @@ const (
 // Event is the representation of an event caused by a node.
 type Event struct {
 	SenderID  uuid.UUID
-	Operation Operation
+	Operation operation
 
-	Origin Member
+	Origin *Member
 
 	Members []*Member
 
 	Time uint64
-	Hops uint
-}
-
-// EventQueue is a thread-safe queue holding events.
-type EventQueue struct {
-	events []Event
-	mutex  *sync.Mutex
-}
-
-// Enqueue adds an event to the tail of the queue.
-func (e *EventQueue) Enqueue(event Event) {
-	e.mutex.Lock()
-	e.events = append(e.events, event)
-	e.mutex.Unlock()
-}
-
-// Dequeue gets an element from the head of the queue and removes it.
-func (e *EventQueue) Dequeue() (event Event) {
-	e.mutex.Lock()
-	event = e.events[0]
-	e.events = e.events[1:]
-	e.mutex.Unlock()
-
-	return event
-}
-
-// Length returns the amount of events currently in the queue.
-func (e *EventQueue) Length() int {
-	return len(e.events)
 }
